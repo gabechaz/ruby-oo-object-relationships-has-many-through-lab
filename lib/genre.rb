28f739e1c8cb9d40ca2fffd0e10119c0dab4a8c1
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-11-20 14:48:56 -0500

    tested has many through lab

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,22 @@
 
+require 'pry'
+
+class Genre
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def songs
+        Song.all.select {|song| song.genre == self}
+    end
+    def artists
+        # binding.pry
+        Song.all.select {|song| song.genre == self}.map {|song| song.artist}
+        
+    end
+end

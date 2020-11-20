82fe13eb6330fbcdd4b5d8907cc4db0edd7cff33
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-11-20 14:48:38 -0500

    commit tree 83d0cf716d2e8ab68981bc3247cae500338a7bd8

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,19 @@
 
+class Song
+    attr_reader :name, :artist, :genre
+    @@all = []
+    def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+end
